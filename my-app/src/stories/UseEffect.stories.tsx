2b8f65dca366d3fbd3d0e390d2262e2f468d175b
YauhenKoltashau <yauhen.koltashau.1985@gmail.com>
2022-06-25 17:35:20 +0300

    Added UseEffect.stories.tsx

--- my-app/src/stories/UseEffect.stories.tsx
+++ my-app/src/stories/UseEffect.stories.tsx
@@ -1,24 +1,70 @@
-import React, {useMemo, useState} from "react"
+import React, {useEffect, useState} from "react"
 
 export default {
-    title: 'UseState demo'
+    title: 'UseEffect demo'
 }
 
-function generateData() {
-    console.log('generateData')
-    let dificultExpression = 1
-    return dificultExpression
-}
 
-export const Example1 = () => {
+export const SimpleExample = () => {
+    console.log('SimpleExample')
 
 
-    const [counter, setCounter] = useState(generateData)
-    const changer = (state: number) => state + 1
+    const [counter, setCounter] = useState(1)
+    const [fakeCounter, setFakeCounter] = useState(1)
+    useEffect(() => {
+        console.log('useEffect every render')
+        document.title = counter.toString()
+
+    })
+    useEffect(() => {
+        console.log('useEffect only first render')
+        document.title = counter.toString()
+
+    }, [])
+    useEffect(() => {
+        console.log('useEffect first render and counter changed')
+        document.title = counter.toString()
+        // api.getUsers().then()
+        // setInterval
+        // indexedDB
+        // document.getElementById
+        // document.title = "User"
+
+    }, [counter])
 
     return <>
-        <button onClick={() => setCounter(changer)}>{counter}</button>
+        <button onClick={() => setCounter(counter + 1)}>counter: {counter}</button>
+        <button onClick={() => setFakeCounter(fakeCounter + 1)}>fakeCounter: {fakeCounter}</button>
 
 
+    </>
+}
+
+export const SetTimeoutExample = () => {
+    console.log('SetTimeoutExample')
+    const [counter, setCounter] = useState(1)
+    const [fakeCounter, setFakeCounter] = useState(1)
+    useEffect(() => {
+        setTimeout(() => {
+            document.title = counter.toString()
+        }, 1000)
+    }, [counter])
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>counter: {counter}</button>
+        <button onClick={() => setFakeCounter(fakeCounter + 1)}>fakeCounter: {fakeCounter}</button>
+
+
+    </>
+}
+
+export const SetIntervalExample = () => {
+    const [counter, setCounter] = useState(0)
+    useEffect(() => {
+        setInterval(()=>{
+           return setCounter((state)=>state + 1)
+        },1000)
+    },[])
+    return <>
+        {counter}
     </>
 }
